e10e4087ff6342718bd3f33a1b0e1d3f7df33214
Hector Manrique Matus Henriquez <manriquematus1@gmail.com>
2018-03-04 20:01:35 -0600

    Segundo commit

--- app/src/main/java/com/example/manrique_matus/peoplepdm/MainActivity.java
+++ app/src/main/java/com/example/manrique_matus/peoplepdm/MainActivity.java
@@ -10,21 +10,27 @@ import android.widget.TextView;
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+    //Creo los atributos de la clase
     public int cantidad=0;
     TextView contador;
     Button btn_1;
     Button btn_2;
     ImageView img;
-
+//La clase onCreate que levantaa la vistaa del xml
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Instancio las variables dentro del onCreate
+
         contador=(TextView)findViewById(R.id.contador);
         btn_1=(Button)findViewById(R.id.add);
         btn_2=(Button)findViewById(R.id.reset);
         img=(ImageView)findViewById(R.id.momazo);
 
+        //setOnClickListener genera que al dar click en el boton se aejecute la funcion onClick dentro de esta
+        //HEY LISTEN BEACH, descifra el codigo y te ganas un punto en el labo :v
         btn_1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
